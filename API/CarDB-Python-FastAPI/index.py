46b42645e1e7f0619864bdd108e5ad8a90246e32
Regan <gregatom99@gmail.com>
2024-11-24 19:39:35 +1100

    API/Python/FastAPI: CORS added

--- API/CarDB-Python-FastAPI/index.py
+++ API/CarDB-Python-FastAPI/index.py
@@ -1,7 +1,14 @@
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
+CORS_ORIGINS = [
+    "http://localhost:3030",
+    "http://localhost:4200/"
+]
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from resources import db
 from Router import router
 
@@ -10,4 +17,11 @@ if(db.DO_INIT):
 
 app = FastAPI()
 app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
