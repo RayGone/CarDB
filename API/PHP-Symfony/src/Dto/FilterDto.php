af146c32aa01ba03e8d59691407b7213ac111702
Regan <gregatom99@gmail.com>
2024-11-27 12:02:15 +1100

    API/Symfony: installing serializer pack; setting up query param dto

---
+++ API/PHP-Symfony/src/Dto/FilterDto.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Dto;
+
+class FilterDto
+{
+    private string $field;
+    private string $ops;
+    private string $value;
+
+    public function __construct(array $param)
+    {
+        $this->fromArray($param);
+    }
+
+    public function getField(): string
+    {
+        return $this->field;
+    }
+
+    public function setField(string $field): self
+    {
+        $this->field = $field;
+        return $this;
+    }
+
+    public function getOps(): string
+    {
+        return $this->ops;
+    }
+
+    public function setOps(string $ops): self
+    {
+        $this->ops = $ops;
+        return $this;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+        return $this;
+    }
+
+    public function fromArray(array $data): self
+    {
+        if (isset($data['field'])) {
+            $this->setField($data['field']);
+        }
+        if (isset($data['ops'])) {
+            $this->setOps($data['ops']);
+        }
+        if (isset($data['value'])) {
+            $this->setValue($data['value']);
+        }
+        
+        return $this;
+    }
+}
