5c626e1c8aab284cebe861acf8e5346ed1f425e3
Regan <gregatom99@gmail.com>
2024-12-02 13:31:23 +1100

    API/Laravel: User create and login routes added

--- API/PHP-Laravel/routes/api.php
+++ API/PHP-Laravel/routes/api.php
@@ -4,6 +4,9 @@ use App\Http\Requests\CreateUserRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\PersonalAccessToken;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,25 +21,45 @@ use App\Models\User;
 
 Route::group([], function(){
     Route::post("/user/create", function(CreateUserRequest $request){
+        $valid_data = $request->validated();
+        $user = User::create([
+            "name" => $valid_data['name'],
+            'email' => $valid_data['email'],
+            'password' => Hash::make($valid_data['password'])
+        ]);
+        $token = $user->createToken('User API Access Token')->plainTextToken;
         return response()->json([
             'message' => 'User created successfully',
-            "user" => $request->all()
-        ]);
-        // return response()->json(['message' => 'User created successfully'], 201);
-        // return response()->json([
-        //     $validatedData["name"],
-        //     $validatedData["email"],
-        //     $validatedData["password"]
-        // ]);
-        // $user = new User($request->all());
-        // $user->createToken('user_access_token_'.$request->get('name'));
+            "user"=>$user, "auth_token"=>$token],
+        201);
     });
 
-    // Route::get("/login", function(Request $request){
+    Route::post("/login", function(Request $request){
+        $login = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        // Attempt to find the user by email
+        $user = User::where('email', $login['email'])->first();
 
-    // });
+        // Check if user exists and if the password is correct
+        if (!$user || !Hash::check($login['password'], $user->password)) {
+            return response()->json(['message' => 'Invalid credentials', "error" => "Incorrect Password!"], 401);
+        }
+        $user->tokens()->delete();
+
+        $token = $user->createToken('Access Token')->plainTextToken;
+
+        return response()->json(["user"=> $user, "auth_token"=>$token]);
+
+    });
 });
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['auth:sanctum'], function(){
+    Route::get('/user', function (Request $request) {
+        // Some how $request->user() && Auth::user() are returning empty;
+        $pas = PersonalAccessToken::findToken($request->bearerToken());
+        $user = $pas->tokenable;
+        return response()->json($user->toArray());
+    });
 });
