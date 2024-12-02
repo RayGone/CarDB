fa6bba5d4d22ca858bb84f03605fc9909ac6726f
Regan <gregatom99@gmail.com>
2024-12-02 17:10:32 +1100

    API/Laravel: Cars Model definition, Migration and Seeder

---
+++ API/PHP-Laravel/app/Models/Cars.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
+
+class Cars extends Model
+{
+    use HasFactory, HasTimestamps;
+
+    /**
+     * @var array<int", "string>
+     */
+    protected $fillable = [
+        "name", "origin", "model_year", "acceleration", "horsepower", "mpg", "weight", "cylinders", "displacement"
+    ];
+
+    /**
+     * @var array<string, string>
+     */
+    protected $casts = [
+
+    ];
+
+    public function getNameAttribute($name){
+        return ucwords($name);
+    }
+
+    public function setNameAttribute($name)
+    {
+        $this->attributes['name'] = mb_strtolower($name);
+    }
+
+    public function getOriginAttribute($origin){
+        return strtoupper($origin);
+    }
+
+    public function setOriginAttribute($origin)
+    {
+        $this->attributes['name'] = mb_strtolower($origin);
+    }
+}
